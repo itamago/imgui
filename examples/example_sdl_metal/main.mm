c9ff162d0b665a793efb6e697189a1d90e6e2ac1
DANHIEZ <pacome.danhiez@nerd.nintendo.com>
2020-01-17 08:27:50 +0100

    [wip] enabled viewports but missing relevant callback on METAL side

--- examples/example_sdl_metal/main.mm
+++ examples/example_sdl_metal/main.mm
@@ -46,9 +46,11 @@ int main(int, char**)
     }
 
     /// Reload default font
+#if 0
     ImFontConfig font_cfg;
     font_cfg.SizePixels = 13.0f * screen->GetRetinaFactor();
     io.Fonts->AddFontDefault(&font_cfg);
+#endif
 
     /// Setup Platform/Renderer bindings
     ImGui_ImplSDL2_InitForMetal(screen->winSDL);
@@ -72,11 +74,11 @@ int main(int, char**)
     {
         countFrame++;
 
-        // Poll and handle events (inputs, window resize, etc.)
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
-        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+        /// Poll and handle events (inputs, window resize, etc.)
+        /// You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+        /// - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
+        /// - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
+        /// Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
         SDL_Event event;
         while (SDL_PollEvent(&event))
         {
